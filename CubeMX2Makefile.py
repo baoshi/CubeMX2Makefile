c9c61970c8aed8866f31bbaabddc5a6a6e475681
Oskar Weigl <oskar.weigl@rapyuta-robotics.com>
2016-05-01 17:33:13 +0200

    Update scanning of source files to match stm32cubeMX 4.14. Contribution by mmprestine

--- CubeMX2Makefile.py
+++ CubeMX2Makefile.py
@@ -3,6 +3,7 @@
 import sys
 import re
 import shutil
+import os
 import os.path
 import string
 import xml.etree.ElementTree
@@ -29,13 +30,14 @@ mcu_regex_to_cflags_dict = {
 }
 
 def main():
+    
     if len(sys.argv) != 2:
         sys.stderr.write("\nSTM32CubeMX project to Makefile V1.6\n")
         sys.stderr.write("-==================================-\n")
         sys.stderr.write("Written by Baoshi <mail\x40ba0sh1.com> on 2015-02-22\n")
         sys.stderr.write("Updated 2015-02-24 for STM32CubeMX 4.13.0 http://www.st.com/stm32cube\n")
         sys.stderr.write("Refer to history.txt for contributors, thanks!\n")
-        sys.stderr.write("Apache License 2.0 <http://www.apache.org/licenses/LICENSE-2.0>\n")
+        sys.stderr.write("Apache License 2.0 <http://www.apachstme3w2e.org/licenses/LICENSE-2.0>\n")
         sys.stderr.write("\nUsage:\n")
         sys.stderr.write("  CubeMX2Makefile.py <STM32CubeMX \"Toolchain Folder Location\">\n")
         sys.exit(C2M_ERR_INVALID_COMMANDLINE)
@@ -56,37 +58,76 @@ def main():
         sys.exit(C2M_ERR_INVALID_COMMANDLINE)
 
     proj_name = os.path.splitext(os.path.basename(proj_folder_path))[0]
-    ac6_project_path = os.path.join(proj_folder_path, 'SW4STM32', proj_name, '.project')
-    ac6_cproject_path = os.path.join(proj_folder_path, 'SW4STM32', proj_name, '.cproject')
+    ac6_project_path = os.path.join(proj_folder_path,'.project')
+    ac6_cproject_path = os.path.join(proj_folder_path,'.cproject')
     if not (os.path.isfile(ac6_project_path) and os.path.isfile(ac6_cproject_path)):
         sys.stderr.write("SW4STM32 project not found, use STM32CubeMX to generate a SW4STM32 project first\n")
         sys.exit(C2M_ERR_NO_PROJECT)
 
-    # .project file
-    try:
-        tree = xml.etree.ElementTree.parse(ac6_project_path)
-    except Exception as e:
-        sys.stderr.write("Unable to parse SW4STM32 .project file: {}. Error: {}\n".format(ac6_project_path, str(e)))
-        sys.exit(C2M_ERR_PROJECT_FILE)
-    root = tree.getroot()
-    nodes = root.findall('linkedResources/link[type=\'1\']/location')
-    sources = []
-    for mcu_node in nodes:
-        sources.append(re.sub(r'^PARENT-2-PROJECT_LOC/', '', mcu_node.text))
-    sources=list(set(sources))
-    sources.sort()
     c_sources_subst = 'C_SOURCES ='
     asm_sources_subst = 'ASM_SOURCES ='
-    for source in sources:
-        ext = os.path.splitext(source)[1]
-        if ext == '.c':
-            c_sources_subst += ' \\\n  ' + source
-        elif ext == '.s':
-            asm_sources_subst += ' \\\n  ' + source
-        else:
-            sys.stderr.write("Unknown source file type: {}\n".format(source))
-            sys.exit(C2M_ERR_IO)
+    as_includes_subst = 'AS_INCLUDES ='
+    c_includes_subst = 'C_INCLUDES ='
+    path2 = ''
+    path2_stored_as = ''
+    first_as = True
+    path2_stored_c = ''
+    first_c = True
+    path2_split = ''
 
+    for path, dirs, files in os.walk(proj_folder_path):
+        for file in files:
+            if file.endswith('.c'):
+                c_sources_subst += ' \\\n  '
+                path2 = os.path.relpath(path,proj_folder_path)
+                path2_split = path2.split('\\')
+                for path2_data in path2_split:
+                    if path2_data == path2_split[0]:
+                        c_sources_subst += path2_data
+                    else:
+                        c_sources_subst += '/' + path2_data
+                c_sources_subst += '/' + file
+            elif file.endswith('.s'):
+                asm_sources_subst += ' \\\n  '
+                path2 = os.path.relpath(path,proj_folder_path)
+                path2_split = path2.split('\\')
+                for path2_data in path2_split:
+                    if path2_data == path2_split[0]:
+                        asm_sources_subst += path2_data
+                    else:
+                        asm_sources_subst += '/' + path2_data
+                asm_sources_subst += '/' + file
+            elif file.endswith('.inc'):
+                path2 = os.path.relpath(path,proj_folder_path)
+                if path2 != path2_stored_as:
+                    path2_stored_as = path2
+                    if first_as:
+                        first_as = False
+                        as_includes_subst += ' -I'
+                    else:
+                        as_includes_subst += '\nAS_INCLUDES += -I'
+                    path2_split = path2.split('\\')
+                    for path2_data in path2_split:
+                        if path2_data == path2_split[0]:
+                            as_includes_subst += path2_data
+                        else:
+                            as_includes_subst += '/' + path2_data                
+            elif file.endswith('.h'):
+                path2 = os.path.relpath(path,proj_folder_path)
+                if path2 != path2_stored_c:
+                    path2_stored_c = path2
+                    if first_c:
+                        first_c = False
+                        c_includes_subst += ' -I'
+                    else:
+                        c_includes_subst += '\nC_INCLUDES += -I'
+                    path2_split = path2.split('\\')
+                    for path2_data in path2_split:
+                        if path2_data == path2_split[0]:
+                            c_includes_subst += path2_data
+                        else:
+                            c_includes_subst += '/' + path2_data
+                    
     # .cproject file
     try:
         tree = xml.etree.ElementTree.parse(ac6_cproject_path)
@@ -111,42 +152,9 @@ def main():
         sys.stderr.write("Unknown MCU: {}\n".format(mcu_str))
         sys.stderr.write("Please contact author for an update of this utility.\n")
         sys.stderr.exit(C2M_ERR_NEED_UPDATE)
-    
-    # Special case for M7, needs to be linked as M4.
-    if 'm7' in ld_subst:
-        ld_subst = mcu_rx_to_cflags_dict['STM32(F|L)4']
-
-    # AS include
-    as_includes_subst = 'AS_INCLUDES ='
-    as_include_path_node_list = root.findall('.//tool[@superClass="fr.ac6.managedbuild.tool.gnu.cross.assembler"]/option[@valueType="includePath"]/listOptionValue')
-    first = True
-    for as_include_path_node in as_include_path_node_list:
-        as_include_path = as_include_path_node.attrib.get('value')
-        if not as_include_path:
-            continue
-        as_include_path = fix_path(as_include_path)
-        if first:
-            as_includes_subst = 'AS_INCLUDES = -I' + as_include_path
-            first = False
-        else:
-            as_includes_subst += '\nAS_INCLUDES += -I' + as_include_path
 
     # AS symbols
     as_defs_subst = 'AS_DEFS ='
-
-    # C include
-    c_includes_subst = 'C_INCLUDES ='
-    c_include_path_node_list = root.findall('.//tool[@superClass="fr.ac6.managedbuild.tool.gnu.cross.c.compiler"]/option[@valueType="includePath"]/listOptionValue')
-    first = True
-    for c_include_path_node in c_include_path_node_list:
-        c_include_path = c_include_path_node.attrib.get('value')
-        if c_include_path:
-            c_include_path = fix_path(c_include_path)
-            if first:
-                c_includes_subst = 'C_INCLUDES = -I' + c_include_path
-                first = False
-            else:
-                c_includes_subst += '\nC_INCLUDES += -I' + c_include_path
 
     # C symbols
     c_defs_subst = 'C_DEFS ='
@@ -165,12 +173,6 @@ def main():
         sys.exit(C2M_ERR_PROJECT_FILE)
     ld_script_name = os.path.basename(ld_script_path)
     ld_script_subst = 'LDSCRIPT = {}'.format(ld_script_name)
-
-    # Copy link script to top level so that user can discard SW4STM32 folder
-    src_path = os.path.join(proj_folder_path, 'SW4STM32', proj_name, ld_script_name)
-    dst_path = os.path.join(proj_folder_path, ld_script_name)
-    shutil.copyfile(src_path, dst_path)
-    sys.stdout.write("File created: {}\n".format(dst_path))
 
     makefile_str = makefile_template.substitute(
         TARGET = proj_name,
